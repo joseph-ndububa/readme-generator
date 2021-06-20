26f4fb6013397633750ff1d28f245af23bd4f172
josephn90 <49831070+josephn90@users.noreply.github.com>
2021-06-20 01:03:32 -0400

    added questions section to readme template

--- Develop/utils/readme-template.js
+++ Develop/utils/readme-template.js
@@ -16,7 +16,6 @@ ${readmeData[0].description}
 * [Tests](#tests)
 * [Questions](#questions)
 
-
 ## Installation
 
 ${readmeData[0].installation}
@@ -27,7 +26,7 @@ ${readmeData[0].usage}
 
 ## License
 
-${readmeData[0].license}
+This application is covered under the ${readmeData[0].license}
 
 ## Contributing
 
@@ -39,7 +38,12 @@ ${readmeData[0].test}
 
 ## Questions
 
+### What is your github username?
 
+You can find me on github at [${readmeData[0].username}] (https://github.com/${readmeData[0].username})
 
+### How can I reach out?
+
+Contact me at <${readmeData[0].email}>
 `;
 }
