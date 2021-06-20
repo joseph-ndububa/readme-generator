c1fbfa2793a5197efee7dfc7394db7dd14fa878c
josephn90 <49831070+josephn90@users.noreply.github.com>
2021-06-19 22:43:42 -0400

    input validation added

--- Develop/index.js
+++ Develop/index.js
@@ -8,47 +8,119 @@ const promptUser = () => {
             type: 'input',
             name: 'title',
             message: 'What is the name of your project?',
+            validate: titleInput => {
+                if (titleInput) {
+                    return true;
+                } else {
+                    console.log('Please enter the name of your project!');
+                    return false;
+                }
+            }
         },
         {
             type: 'input',
             name: 'description',
             message: 'Please provide a description of your project.',
+            validate: descriptionInput => {
+                if (descriptionInput) {
+                    return true;
+                } else {
+                    console.log('Please provide a description of your project!');
+                    return false;
+                }
+            }
         },
         {
             type: 'input',
             name: 'installation',
-            message: 'Please provide installation instructions for your project.',
+            message: 'Please provide installation instructions for your application.',
+            validate: installInput => {
+                if (installInput) {
+                    return true;
+                } else {
+                    console.log('Please provide installation instructions for your application!');
+                    return false;
+                }
+            }
         },
         {
             type: 'input',
             name: 'usage',
             message: 'Please provide usage instructions for your application.',
+            validate: usageInput => {
+                if (usageInput) {
+                    return true;
+                } else {
+                    console.log('Please provide usage instructions for your application!');
+                    return false;
+                }
+            }
         },
         {
             type: 'input',
             name: 'contribution',
             message: 'How can other developers contribute to the success of this project?',
+            validate: contributionInput => {
+                if (contributionInput) {
+                    return true;
+                } else {
+                    console.log('Please describe how other developers can contribute to this project!');
+                    return false;
+                }
+            }
         },
         {
             type: 'input',
             name: 'test',
             message: 'How can users test your application?',
+            validate: testInput => {
+                if (testInput) {
+                    return true;
+                } else {
+                    console.log('Please describe how users can test your application!');
+                    return false;
+                }
+            }
         },
         {
             type: 'list',
             name: 'license',
             message: 'Choose a license.',
-            choices: ['Apache License 2.0', 'BSD 3-Clause "New" or "Revised" license', 'BSD 2-Clause "Simplified" or "FreeBSD" license', 'GNU General Public License (GPL)', 'GNU General Public License (GPL)', 'MIT license', 'Mozilla Public License 2.0', 'Common Development and Distribution License', 'Eclipse Public License version 2.0']
+            choices: ['Apache License 2.0', 'BSD 3-Clause "New" or "Revised" license', 'BSD 2-Clause "Simplified" or "FreeBSD" license', 'GNU General Public License (GPL)', 'GNU General Public License (GPL)', 'MIT license', 'Mozilla Public License 2.0', 'Common Development and Distribution License', 'Eclipse Public License version 2.0'],
+            validate: licenseInput => {
+                if (licenseInput) {
+                    return true;
+                } else {
+                    console.log('Please choose a license!');
+                    return false;
+                }
+            }
         },
         {
             type: 'input',
             name: 'username',
             message: 'What is your github username?',
+            validate: usernameInput => {
+                if (usernameInput) {
+                    return true;
+                } else {
+                    console.log('Please enter your github username!');
+                    return false;
+                }
+            }
         },
         {
             type: 'input',
             name: 'email',
             message: 'What is your email address?',
+            validate: emailInput => {
+                if (emailInput) {
+                    return true;
+                } else {
+                    console.log('Please enter your email address!');
+                    return false;
+                }
+            }
         }
     ])
 }
